ea9054574dc6330fb7c8e0c38b8395603fb392e1
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-01-17 02:38:08 +0200

    Auto-commit 17.01.2026  2:38:08,42

--- source/Application.lua
+++ source/Application.lua
@@ -1,3 +1,2 @@
 local data = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/Okayevls/unknown-heaven/commits/main"))
-local rawUrl = "https://raw.githubusercontent.com/Okayevls/unknown-heaven/"..data["sha"].."/source/Bootstrap.lua?v="..os.time()
-loadstring(game:HttpGet(rawUrl))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/unknown-heaven/"..data["sha"].."/source/Bootstrap.lua?v="..os.time()))()
