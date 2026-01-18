bf2b63769f44f4baa142fff8c4c0360116bb1e13
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-01-18 13:34:38 +0200

    Auto-commit 18.01.2026 13:34:38,20

--- source/Bootstrap.lua
+++ source/Bootstrap.lua
@@ -1,12 +1,14 @@
 local data = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/Okayevls/unknown-heaven/commits/main"))
-
+print("Heaven: Step - 1 (Loading Data...)")
 local downloader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/unknown-heaven/"..data["sha"].."/source/util/other/FastDownloader.lua?v="..os.time()))()
-
+print("Heaven: Step - 1 (Loading Utility...)")
 
 local inject = downloader.new("Okayevls", "unknown-heaven", "main"):GetLatestSHA()
+print("Heaven: Step - 2 (Loading Injector...)")
 
 --Utility
 local RenderUtil = inject:Load("source/util/render/RenderUtil.lua")
+print("Heaven: Step - 3 (Loading Utility...)")
 
 local ctx = {
     Inject = inject,
@@ -15,5 +17,6 @@ local ctx = {
 
 --Main
 inject:Load("source/panel/loaderGui/LoaderPanel.lua", ctx)
+print("Heaven: Step - 4 (Loading Main...)")
 
 print("Heaven: Welcome")
