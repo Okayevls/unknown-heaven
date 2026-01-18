da076df0bbf6d292cbef9df068c644ae12995c5b
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-01-18 19:53:54 +0200

    Auto-commit 18.01.2026 19:53:54,09

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
 cd /d E:\LedikAll\unknown-heaven
 
+
+echo 🔄 Добавление изменений...
 git add . >nul 2>&1
+echo 💾 Создание коммита...
 git commit -m "Auto-commit %date% %time%" > commit.log 2>&1
+echo 🚀 Отправка в репозиторий...
 git push origin main >nul 2>&1
