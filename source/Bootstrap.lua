53b6f0c04c715802f4d059c031d1a938b6b59dc1
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-02-03 13:13:31 +0200

    Auto-commit 03.02.2026 13:13:29,78

--- source/Bootstrap.lua
+++ source/Bootstrap.lua
@@ -1,23 +1,18 @@
 local data = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/Okayevls/unknown-heaven/commits/main"))
 print("Heaven: Step - 1 (Loading Data...)")
 local downloader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/unknown-heaven/"..data["sha"].."/source/util/other/FastDownloader.lua?v="..os.time()))()
-print("Heaven: Step - 1 (Loading Utility...)")
+print("Heaven: Step - 2 (Loading Utility...)")
 
 local inject = downloader.new("Okayevls", "unknown-heaven", "main"):GetLatestSHA()
-print("Heaven: Step - 2 (Loading Injector...)")
+print("Heaven: Step - 3 (Loading Injector...)")
 
 --Utility
-local Service = inject:Load("source/util/offsets/Service.lua")
-print("Heaven: Step - 3 (Loading Utility...)")
+--local Service = inject:Load("source/util/offsets/Service.lua")
+print("Heaven: Step - 4 (Loading Utility...)")
 
 getgenv().ctx = {
     Inject = inject,
-    Service = Service,
 }
-
---Main
-inject:Load("source/panel/loaderGui/LoaderPanel.lua")
-print("Heaven: Step - 4 (Loading Main...)")
 
 --Main
 inject:Load("source/panel/mainGui/ClickGui.lua")
