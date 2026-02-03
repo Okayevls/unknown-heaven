207a9868f3cbe65275f17bcdd28bfd30f1e27a4e
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-02-03 17:31:21 +0200

    Auto-commit 03.02.2026 17:31:21,37

--- source/Application.lua
+++ source/Application.lua
@@ -1,21 +1,11 @@
 local http = game:GetService("HttpService")
-
 local success, data = pcall(function()
     return http:JSONDecode(game:HttpGet("https://api.github.com/repos/Okayevls/unknown-heaven/commits/main"))
 end)
 
-if success and data and data.sha then
-    local sha = data.sha
-    getgenv().LatestSHA = sha
-    local url = string.format("https://raw.githubusercontent.com/Okayevls/unknown-heaven/%s/source/Bootstrap.lua?v=%s", sha, os.time())
-
-    local ok, err = pcall(function()
-        loadstring(game:HttpGet(url))()
-    end)
-
-    if not ok then
-        warn("[Heaven]: Critical error during Bootstrap execution: " .. tostring(err))
-    end
+if success and data["sha"] then
+    getgenv().LatestSHA = data["sha"]
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Okayevls/unknown-heaven/"..data["sha"].."/source/Bootstrap.lua?v="..os.time()))()
 else
-    warn("[Heaven]: Failed to fetch latest commit SHA. Check your internet or GitHub API limits.")
+    warn("Heaven: Failed to fetch latest commit SHA")
 end
