637c6af936630ad72229ace7d57608c29913f10f
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-02-03 16:55:33 +0200

    upd

--- scripts/RoStreet/module/Test.lua
+++ scripts/RoStreet/module/Test.lua
@@ -1,39 +1,18 @@
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-
-local connection
+local TextChatService = game:GetService("TextChatService")
 
 return {
-    Name = "FlyUp",
-    Desc = "Плавно поднимает игрока вверх, пока включен.",
-    Class = "Movement",
+    Name = "ChatSpy",
+    Desc = "Вы можете подглядывать за отключенным чатом",
+    Class = "Player",
     Category = "Utility",
 
-    Settings = {
-        {Type="Slider", Name="Speed", Default=5, Min=1, Max=50, Step=1},
-    },
+    Settings = {},
 
     OnEnable = function(ctx)
-        local player = Players.LocalPlayer
-
-        connection = RunService.Heartbeat:Connect(function(dt)
-            local character = player.Character
-            local hrp = character and character:FindFirstChild("HumanoidRootPart")
-
-            if hrp then
-                local speed = ctx:GetSetting("Speed")
-                hrp.CFrame = hrp.CFrame + Vector3.new(0, speed * dt, 0)
-            end
-        end)
-
-        print("[FlyUp] Enabled")
+        TextChatService.ChatWindowConfiguration.Enabled = true
     end,
 
     OnDisable = function(ctx)
-        if connection then
-            connection:Disconnect()
-            connection = nil
-        end
-        print("[FlyUp] Disabled")
+        TextChatService.ChatWindowConfiguration.Enabled = false
     end,
 }
