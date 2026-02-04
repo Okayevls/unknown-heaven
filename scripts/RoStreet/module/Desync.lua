71b2795d875996717b2aa9707d2d713651d94f85
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-02-04 21:28:54 +0200

    Auto-commit 04.02.2026 21:28:53,21

--- scripts/RoStreet/module/Desync.lua
+++ scripts/RoStreet/module/Desync.lua
@@ -23,20 +23,35 @@ local function resetCamera()
     end
 end
 
-local function getGroundLevel(pos)
-    local rayOrigin = pos + Vector3.new(0, 5, 0)
-    local rayDirection = Vector3.new(0, -500, 0)
+local function getGroundLevel()
+    local char = game.Players.LocalPlayer.Character
+    local root = char and char:FindFirstChild("HumanoidRootPart")
+    local humanoid = char and char:FindFirstChildOfClass("Humanoid")
+    if not root or not humanoid then return nil end
 
-    local params = RaycastParams.new()
-    params.FilterDescendantsInstances = {LocalPlayer.Character}
-    params.FilterType = Enum.RaycastFilterType.Exclude
+    local rayParams = RaycastParams.new()
+    rayParams.FilterDescendantsInstances = {char}
+    rayParams.FilterType = Enum.RaycastFilterType.Exclude
+    rayParams.IgnoreWater = false
+    rayParams.RespectCanCollide = true
 
-    local result = workspace:Raycast(rayOrigin, rayDirection, params)
+    local currentOrigin = root.Position
+    local traveled = 0
+    local maxDistance = 1e5
 
-    if result then
-        return result.Position.y
+    while traveled < maxDistance do
+        local result = workspace:Raycast(currentOrigin, Vector3.new(0, -5000, 0), rayParams)
+
+        if result then
+            local legOffset = (root.Size.Y / 2) + humanoid.HipHeight
+            return result.Position.y + legOffset
+        end
+
+        currentOrigin = currentOrigin - Vector3.new(0, 5000, 0)
+        traveled = traveled + 5000
     end
-    return pos.y
+
+    return root.Position.y
 end
 
 local _connections = {}
@@ -47,6 +62,8 @@ local maxWait = 3
 local nextFlick = tick() + math.random(minWait, maxWait)
 local isFlicking = false
 local flickEnd = 0
+local flagThreshold = 50
+
 return {
     Name = "Desync",
     Desc = "Дает огромнейшие преимущество над игроками",
@@ -76,7 +93,7 @@ return {
 
                         local randomOffset = Vector3.new(rootPart.Position.x, math.random(ctx:GetSetting("MinY"), ctx:GetSetting("MaxY")), rootPart.Position.z)
                         if isFlicking then
-                            local groundY = ctx:GetSetting("Calculate Ground") and getGroundLevel(rootPart.Position) or rootPart.Position.y
+                            local groundY = ctx:GetSetting("Calculate Ground") and getGroundLevel() or rootPart.Position.y
                             desync.teleportPosition = Vector3.new(rootPart.Position.x,  groundY, rootPart.Position.z)
                             if currentTime >= flickEnd then
                                 isFlicking = false
@@ -93,7 +110,15 @@ return {
                         RunService.RenderStepped:Wait()
 
                         desync_setback.CFrame = desync.old_position * CFrame.new(0, rootPart.Size.Y / 2 + 0.5, 0)
-                        rootPart.CFrame = desync.old_position
+
+
+                        if (rootPart.Position.y > desync.old_position.y + flagThreshold) then
+                            local groundPos = getGroundLevel()
+                            rootPart.CFrame = CFrame.new(rootPart.Position.x, groundPos, rootPart.Position.z) * (desync.old_position - desync.old_position.Position)
+                            warn("⚠️ [Heaven] Anti-Cheat Setback Fix Applied")
+                        else
+                            rootPart.CFrame = desync.old_position
+                        end
                     end
                 end
             end))
