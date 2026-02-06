cebedeecd7285d74ddc373486470b27e8990cb7d
Okayevls <151445860+Okayevls@users.noreply.github.com>
2026-02-06 14:02:55 +0200

    Auto-commit 06.02.2026 14:02:54,90

--- scripts/RoStreet/module/Hud.lua
+++ scripts/RoStreet/module/Hud.lua
@@ -76,7 +76,7 @@ return {
     OnEnable = function(ctx)
         local playerGui = player:WaitForChild("PlayerGui")
 
-        bgGui = create("ScreenGui", { Name = "HeavenHud_BG", ResetOnSpawn = false, IgnoreGuiInset = true, DisplayOrder = -1 }, playerGui)
+        bgGui = create("ScreenGui", { Name = "HeavenHud_BG", ResetOnSpawn = false, IgnoreGuiInset = true, DisplayOrder = 2147483643 }, playerGui)
         fgGui = create("ScreenGui", { Name = "HeavenHud_FG", ResetOnSpawn = false, IgnoreGuiInset = true, DisplayOrder = 2147483646 }, playerGui)
 
         activeNotifs = {}
